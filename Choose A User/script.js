bb0757c53faa0197979b69985781bee803372974
Eslam Gamal <geslam587@gmail.com>
2023-04-06 16:27:50 +0200

    wrapping up

--- Choose A User/script.js
+++ Choose A User/script.js
@@ -2,16 +2,19 @@
 function numberChoose() { document.querySelector("h4").innerHTML =`you will add ${document.querySelectorAll(".shadow").length} users` 
                             if (document.querySelectorAll(".shadow").length == 6 ) {
                                 document.getElementsByClassName("button")[0].classList.add("checkall")
-                                  }
+                                document.getElementsByClassName("button")[0].innerHTML = "Deselect all"
+                                
+                            }
                             else{
-                                     document.getElementsByClassName("button")[0].classList.remove("checkall")
+                                document.getElementsByClassName("button")[0].classList.remove("checkall")
+                                document.getElementsByClassName("button")[0].innerHTML = "select all"
                                  }
 }
 
 function check() {
     let allcards=  document.getElementsByClassName("card")
     let allcheck= document.getElementsByClassName("check")
-
+    
     if ( !document.getElementsByClassName("button")[0].classList.contains("checkall")) {
         console.log(123);
         for (let cardd of allcards) {cardd.classList.add("shadow")}
@@ -24,10 +27,12 @@ function check() {
     }
     if (!document.getElementsByClassName("button")[0].classList.contains("checkall")) {
         document.getElementsByClassName("button")[0].classList.add("checkall")
+        document.getElementsByClassName("button")[0].innerHTML = "Deselect all"
         numberChoose()
     }
     else{
         document.getElementsByClassName("button")[0].classList.remove("checkall")
+        document.getElementsByClassName("button")[0].innerHTML = "select all"
         numberChoose()
     }
 }
